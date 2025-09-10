77e459a8a93bd0905be9b587852b6dd1ed9f5508
Maruthi P <maruthip266@gmail.com>
2025-09-10 18:13:31 +0530

    Add files via upload
    Create interface of library Management

---
+++ libManagement.java
@@ -0,0 +1,10 @@
+package java_real;
+
+interface libManagement {
+public static final int bookCoount = 0;
+public void init();
+public void checkin();
+public void checkout();
+public void displayStatus();
+
+}
